f17e13a012f3156eb2757456c991025423b995f3
fatimamohamed <adropofilm@gmail.com>
2018-02-27 19:50:52 -0600

    first commit

--- src/SimpleGui.java
+++ src/SimpleGui.java
@@ -1,14 +1,28 @@
 import javax.swing.*;
+import java.awt.event.*; // ActionListener and ActionEvent are in here ;)
 
-public class SimpleGui {
-
+public class SimpleGui implements ActionListener {
+	JButton button;
+	
 	public static void main(String[] args) {
+		SimpleGui gui = new SimpleGui();
+		gui.go();
+	}
+	
+	public void go() {
 		JFrame frame = new JFrame();
-		JButton button = new JButton("click me");
+		button = new  JButton("click me");
 		
-		frame.setSize(200, 200);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		button.addActionListener(this);	// ADD ME TO YOUR LIST OF LISTENERS, BUTTON
+		
 		frame.getContentPane().add(button);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(200, 200);
 		frame.setVisible(true);
 	}
+	
+	public void actionPerformed(ActionEvent event) {		// IMPLEMENT ACTIONLISTENER INTERFACE'S ACTIONPERFORMED()
+		button.setText("I've been clicked"); 	// STUFF JUST POPPED OFF SHAWTY
+	}
+	
 }
